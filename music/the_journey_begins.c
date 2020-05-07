5f98c714b402c70ef6bf17075149ed58db9b5416
Sebastian Riedel <git@basxto.de>
2020-05-07 14:17:46 +0200

    Port full famitracker version of the journey begins

--- music/the_journey_begins.c
+++ music/the_journey_begins.c
@@ -6,49 +6,9 @@
 // by Igor Gundarev released under cc0
 
 const Pattern_frame the_journey_begins_pattern[][8] = {
-    //  |-------PULSE-1-------|
-    //  |------NOTE-----| |VI-|
-    {
-        {0x30 | note_f  , 0x60},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0x30 | note_gis, 0x60},
-        {0xFF           , 0x6F}
-    },
-    {
-        {0x30 | note_cis, 0x60},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F}
-    },
-    {
-        {0x30 | note_dis, 0x60},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x6F},
-        {0x30 | note_cis, 0x60},
-        {0xFF           , 0x6F}
-    },
-    {
-        {0x30 | note_c  , 0x60},
-        {0xFF           , 0x6F},
-        {0xFF           , 0x00},
-        {0xFF           , 0x00},
-        {0xFF           , 0x00},
-        {0x30 | note_fis, 0x60},
-        {0x30 | note_ais, 0x60},
-        {0x30 | note_gis, 0x80}
-    },
+
     //  |-------PULSE-2-------|
+    //  |------NOTE-----| |VI-|
     {
         {0x10 | note_cis, 0x80},
         {0xFF           , 0x8F},
@@ -121,18 +81,145 @@ const Pattern_frame the_journey_begins_pattern[][8] = {
         {0x86           , 0x00},
         {0xA4           , 0x00}
     },
+    //  |-------PULSE-1-------|
+    {
+        {0x20 | note_f  , 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0x20 | note_gis, 0x60},
+        {0xFF           , 0x6F}
+    },
+    {
+        {0x30 | note_cis, 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F}
+    },
+    {
+        {0x30 | note_dis, 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0x30 | note_cis, 0x60},
+        {0xFF           , 0x6F}
+    },
+    {
+        {0x30 | note_c  , 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0x20 | note_fis, 0x60},
+        {0x20 | note_ais, 0x60},
+        {0x30 | note_gis, 0x80}
+    },
+    {
+        {0x30 | note_f  , 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00}
+    },
+    {
+        {0x30 | note_f  , 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0x30 | note_cis, 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F}
+    },
+    {
+        {0x20 | note_ais, 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0x20 | note_gis, 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F}
+    },
+    {
+        {0x30 | note_cis, 0x60},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x6F},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00}
+    },
+    {
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00}
+    },
+    {
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0x20 | note_cis, 0x60},
+        {0xFF           , 0x6F},
+        {0x20 | note_ais, 0x60},
+        {0xFF           , 0x6F},
+        {0x20 | note_gis, 0x60},
+        {0xFF           , 0x6F}
+    },
+    {
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0xFF           , 0x00},
+        {0x20 | note_cis, 0x60},
+        {0x20 | note_ais, 0x60},
+        {0x20 | note_gis, 0x60}
+    }
 };
 
 const Song_frame the_journey_begins_arrangement[] = {
 //   P1  P2  W   N
-    { 0,  4,  7, 10},
-    { 1,  4,  7, 10},
-    { 2,  5,  8, 10},
-    { 3,  5,  8, 10},
-    { 0,  6,  7, 10},
-    { 1,  6,  7, 10},
-    { 2,  6,  9, 10},
-    { 3,  6,  9, 10}
+    { 7,  0,  3,  6},
+    { 8,  0,  3,  6},
+    { 9,  1,  4,  6},
+    {10,  1,  4,  6},
+    { 7,  2,  3,  6},
+    { 8,  2,  3,  6},
+    {15,  2,  5,  6},
+    {17,  2,  5,  6},
+    { 7,  0,  3,  6},
+    { 8,  0,  3,  6},
+    { 9,  1,  4,  6},
+    {11,  1,  4,  6},
+    {12,  2,  3,  6},
+    {13,  2,  3,  6},
+    {14,  2,  5,  6},
+    {15,  2,  5,  6},
+    {15,  0,  3,  6},
+    {15,  0,  3,  6},
+    {15,  1,  4,  6},
+    {15,  1,  4,  6},
+    {15,  2,  3,  6},
+    {15,  2,  3,  6},
+    {15,  2,  5,  6},
+    {16,  2,  5,  6}
 };
 
 const Instrument the_journey_begins_instruments[] = {
@@ -149,7 +236,7 @@ const Instrument the_journey_begins_instruments[] = {
     {0x00 |5,   30, 0xC0, 0x20 | 0x04 | 0x03}
 };
 
-Song the_journey_begins={8,8,5,
+Song the_journey_begins={8,24,5,
     the_journey_begins_pattern[0],
     the_journey_begins_arrangement,
     the_journey_begins_instruments
